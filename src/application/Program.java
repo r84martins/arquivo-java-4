6973757159174dfdb47aa629a1ac03362ce1362f
Ricardo Martins <rs84martins@gmail.com>
2020-07-06 20:59:25 -0300

    Listando arquivos e pastas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		//Lista todos os diretórios a partir de um caminho informado pelo usuário
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory); //listar somente diretório ou pasta
+		
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		//Lista todos os arquivos a partir de um caminho informado pelo usuário
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		//Cria uma subpasta chamada dir
+		boolean sucess = new File(strPath + "\\subdir").mkdir(); 
+		System.out.println("Directory created successfully: " + sucess);
+		
+		sc.close();
 	}
-
 }
